c0a4e91bf9059fe5e68e31e629e3c13fb3f246e9
Kevin Ford <kevin.sean.ford@gmail.com>
2020-09-03 18:18:21 -0400

    Added problem 9.4

---
+++ 2of4_python_data_structures/problem9.4.py
@@ -0,0 +1,26 @@
+# 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
+# problem stem:
+# name = input("Enter file:")
+# if len(name) < 1 : name = "mbox-short.txt"
+# handle = open(name)
+
+# solution
+d = dict()
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+for line in handle:
+  if line == '\n':
+    continue
+  linelist = line.split()
+  if linelist[0] == 'From':
+    d[linelist[1]] = d.get(linelist[1], 0) + 1
+largest = 0
+for key in d:
+  if d[key] > largest:
+    largest = d[key]
+for key in d:
+  if d[key] == largest:
+    print(key, d[key])
+
