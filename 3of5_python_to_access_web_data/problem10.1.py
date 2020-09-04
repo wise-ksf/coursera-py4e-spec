6ab6c6b14103d4860015526228180a618a6c7200
Kevin Ford <kevin.sean.ford@gmail.com>
2020-09-04 10:47:42 -0400

    New dir for course 3. First two problems complete. Some notes.

---
+++ 3of5_python_to_access_web_data/problem10.1.py
@@ -0,0 +1,6 @@
+# Peer Review: Installing and Running Python Screen Shots
+
+# Install Python and a programming text editor and write a program that prints one line other than 'hello world', then take two screen shots and upload them below. You should use the command line to execute the Python program you wrote in the text editor. Please do *not* use the IDLE Python Shell, the Python Interpreter (>>>), or a shortcut in your text editor to run the code. Later in the class when we start reading files, we will need to be able to run Python programs from particular directories. See the videos for details.
+# This course uses a third-party tool, Peer Review: Installing and Running Python Screen Shots, to enhance your learning experience. The tool will reference basic information like your name, email, and Coursera ID.
+
+print("Hi, I'm Kevin Ford the submariner! I'm using vim as my text editor and python3.8.5 on MacOS, installed using brew.")
